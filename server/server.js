5541491fbb736be5f57c48a4e9d3688ccc136ff7
Siddharth Dalbhanjan <siddharthdalbhanjan22@gmail.com>
2025-04-25 00:38:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const api = require('./routes/api');
 const app = express();
 app.use(express.json()); // necessary to parse JSON request body
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(bodyParser.json()); 
 
@@ -16,18 +15,8 @@ app.use('/api', api);
 // 👇 This is the key part
 app.use(express.static(path.join(__dirname, '../dist/event/browser')));
 
-app.get('/test.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/event/browser/index.html'));
-});
-
-// Optional: Example API  
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
-});
-
 // 👇 For Angular routing
 app.get('*', (req, res) => {
-  console.log("Wildcard route called for:", req.url);
   res.sendFile(path.join(__dirname, '../dist/event/browser/index.html'));
 });
 
